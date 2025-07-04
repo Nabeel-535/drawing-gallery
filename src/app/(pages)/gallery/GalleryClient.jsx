766b003e42223951701245b3680e651193b11a63
Muhammad Rehan <raorehanweb@gmail.com>
2025-07-04 11:03:19 +0500

    more post at a time

--- src/app/(pages)/gallery/GalleryClient.jsx
+++ src/app/(pages)/gallery/GalleryClient.jsx
@@ -17,7 +17,7 @@ export default function GalleryClient() {
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
   const [totalPosts, setTotalPosts] = useState(0);
-  const [postsPerPage] = useState(20); // Number of posts per page
+  const [postsPerPage] = useState(50); // Number of posts per page
   
   const searchParams = useSearchParams();
 
